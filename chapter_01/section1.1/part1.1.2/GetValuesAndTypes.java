81e98024d728dc3749737bc9fbe59738387b5dd6
Wangrong23 <1902167838@qq.com>
2018-09-25 15:36:34 +0800

    update

---
+++ chapter_01/section1.1/part1.1.2/GetValuesAndTypes.java
@@ -0,0 +1,11 @@
+
+public class GetValuesAndTypes{
+    public static void main(String[] args) {
+        //输出“(1+2.236)/2”，“1+2+3+4.0”，“4.1>=4”，“1+2+"3"”的值和类型
+        //TODO type
+        System.out.println((1+2.236)/2);
+        System.out.println(1+2+3+4.0);
+        System.out.println(4.1>=4);
+        System.out.println(1+2+"3");
+    }
+}
