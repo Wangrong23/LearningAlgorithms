baaa9f0d335847dd3138929925384ed0b8453b04
unknown <Wangrong23>
2018-10-04 18:30:14 +0800

    win test

--- chapter_01/section1.1/part1.1.1/GetValues.java
+++ chapter_01/section1.1/part1.1.1/GetValues.java
@@ -1,6 +1,6 @@
 public class GetValues{
     public static void main(String[] args){
-        //输出“(0+15)/2”，“2.0e-6*100000000.1”，“true&&false || true&&true”的值
+        //输出“(0+15)/2”， “2.0e-6*100000000.1”， “true&&false || true&&true”的值
         System.out.println((0+15)/2);
         System.out.println(2.0e-6*100000000.1);
         System.out.println(true&&false || true&&true);
